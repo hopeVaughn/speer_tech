33bbc96ffead08803e86bbcb236a117b39af00bd
hopeVaughn <hopevaughnwarren@gmail.com>
2023-08-03 16:18:28 -0700

    fix the styling on archive element

--- src/Components/AllCalls.tsx
+++ src/Components/AllCalls.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ActivityItem from './ActivityItem';
 import { callData } from '../utils/data';
+import Archive from './Archive';
 
 const ActivityFeed: React.FC = () => {
   return (
-    <section className="relative mt-12 flex flex-col overflow-auto h-[60vh]">
+    <section className="relative mt-12 flex flex-col overflow-auto h-[70vh]">
+      <div className="sticky top-0 z-10">
+        <Archive />
+      </div>
       <ActivityItem data={callData} onlyArchived={false} />
     </section>
   );
--- src/Components/Archive.tsx
+++ src/Components/Archive.tsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import { CallProps } from '../utils/propTypes'
-import { VscArchive } from 'react-icons/vsc'
+import React from 'react';
+import { VscArchive } from 'react-icons/vsc';
 
-const Archive: React.FC<CallProps> = () => {
+const Archive: React.FC = () => {
   return (
-    <section className="rounded-b-lg w-3/5 h-12 mx-auto flex justify-center items-center bg-white shadow-md px-4 py-1 mt-2">
-      <button className="flex justify-center items-center w-full">
-        <VscArchive size={24} className="mr-16 hover:text-custom-green cursor-pointer" />
-        <span>Archive all messages</span>
+    <section className="rounded-b-lg min-w-[16rem] max-w-[16rem] h-12 mx-auto flex justify-center items-center bg-white shadow-md px-4 py-1 mt-2">
+      <button className="flex flex-row justify-center items-center w-full text-center">
+        <VscArchive size={24} className="mr-2 hover:text-custom-green cursor-pointer" />
+        <span className="text-xs">Archive all messages</span>
       </button>
     </section>
-  )
-}
+  );
+};
 
-export default Archive
+export default Archive;
